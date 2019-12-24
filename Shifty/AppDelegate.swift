2a2e6fdeee6f1db68f3991a683b5e9bc32f5f7fa
Graham Place <grahamgplace@gmail.com>
2019-12-24 09:19:21 -0800

    Initial Commit

---
+++ Shifty/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Shifty
+//
+//  Created by Graham Place on 12/24/19.
+//  Copyright © 2019 Graham Place. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Shifty/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Shifty
+//
+//  Created by Graham Place on 12/24/19.
+//  Copyright © 2019 Graham Place. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
